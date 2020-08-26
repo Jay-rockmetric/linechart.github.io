0e169b921076372d4c01e263f8516ed2d685e040
Jay-rockmetric <Jay-rockmetric.noreply@github.com>
2020-08-26 12:13:14 +0530

    Initialize Build

---
+++ precache-manifest.f0f95e705d8cd57a9f666e7224573266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170a599436a3c98dc73ecbb6894da044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96b1ae6e29526ec80c28",
+    "url": "/static/css/2.93e6f2b2.chunk.css"
+  },
+  {
+    "revision": "7d47c293ac80fb78a9a9",
+    "url": "/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "96b1ae6e29526ec80c28",
+    "url": "/static/js/2.43c84452.chunk.js"
+  },
+  {
+    "revision": "d679e4066658f44ff9c3e023bdda957e",
+    "url": "/static/js/2.43c84452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d47c293ac80fb78a9a9",
+    "url": "/static/js/main.4e4f887b.chunk.js"
+  },
+  {
+    "revision": "995316c1b775073875f3",
+    "url": "/static/js/runtime-main.c7e5dd6a.js"
+  }
+]);
